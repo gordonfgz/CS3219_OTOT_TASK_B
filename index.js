1e2ab7a9d7c819b24830e34740f25dccc8d4b391
gordonfgz <gordonfgz@gmail.com>
2021-09-27 21:19:28 +0800

    add start to script

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
