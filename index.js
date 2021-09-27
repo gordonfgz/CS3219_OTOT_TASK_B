d782191c07071f32328e406bc3f7b6ab2208dd5f
gordonfgz <gordonfgz@gmail.com>
2021-09-27 21:08:58 +0800

    add more params

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
