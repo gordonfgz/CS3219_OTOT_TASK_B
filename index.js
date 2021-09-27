99b32488bcaede6bb7c1a3929899758e2653af83
gordonfgz <gordonfgz@gmail.com>
2021-09-27 21:37:18 +0800

    add app.use(express.json()));

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 //const cors = require("cors");
 
 const app = express();
-//app.use(express.json());
+app.use(express.json());
 //app.use(cors());
 
 const PORT = process.env.PORT || 3000;
@@ -10,6 +10,11 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/', (req, res) => {
+    output = req.body
+    res.send(body)
+  })
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
