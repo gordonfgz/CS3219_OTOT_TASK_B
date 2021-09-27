1028c6f19c303e3b5216473fcf9d5bbdcfa1dbf0
gordonfgz <gordonfgz@gmail.com>
2021-09-27 21:57:49 +0800

    add users and get request and post request

--- index.js
+++ index.js
@@ -3,17 +3,35 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 //app.use(cors());
 
 const PORT = process.env.PORT || 3000;
+
+const users = [
+    { name: 'Jeremy', age: 22},
+    { name: 'Kelvin', age: 21}
+]
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/users', (req, res) => {
+    res.status(200).send(users)
+})
+
 app.post('/', (req, res) => {
-    res.send(req.body)
-  })
+    try {
+        const user = req.body;
+        users.push(user);
+        res.status(201).send(users);
+    } catch (e) {
+        console.log(e.message)
+        res.status(404).send("error")
+    }
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
